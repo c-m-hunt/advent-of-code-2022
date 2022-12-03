a29659a53e0cbc95c3d538aa3028e611fd969308
Chris Hunt <chris.hunt@hudl.com>
2022-12-03 10:16:30 +0000

    refactor: minor change on day 3

--- 3.py
+++ 3.py
@@ -8,16 +8,13 @@ priorities = {l: i + 1 for i, l in enumerate(
 
 total = 0
 for bp in lines:
-    bp1 = bp[:len(bp) // 2]
-    bp2 = bp[len(bp) // 2:]
-
+    bp1, bp2 = bp[:len(bp) // 2], bp[len(bp) // 2:]
     intersect = set(bp1).intersection(set(bp2))
     total += priorities[list(intersect)[0]]
 
 print("Part 1:", total)
 
-i = 0
-total = 0
+i, total = 0, 0
 while i < len(lines):
     intersect1 = set(lines[i]).intersection(set(lines[i + 1]))
     intersect2 = intersect1.intersection(set(lines[i + 2]))
