5723852aefab441f9485f15dafb8ce10771ab64d
Chris Hunt <chris.hunt@hudl.com>
2022-12-08 09:02:25 +0000

    feat: day 8

---
+++ day8.py
@@ -0,0 +1,60 @@
+from typing import List
+import numpy as np
+import utils
+
+DAY = 8
+
+
+def load_and_parse_data(day: int, test: bool = False) -> List[str]:
+    data = utils.get_input(day, test)
+    return np.array([[int(i) for i in line] for line in data])
+
+
+def solve_part_1(data):
+    count = 0
+    height = data.shape[0]
+    width = data.shape[1]
+    for i in range(height):
+        for j in range(width):
+            if i == 0 or j == 0 or i == height - 1 or j == width - 1:
+                count += 1
+            else:
+                tree = data[i][j]
+                if (np.amax(data[i, 0:j]) < tree or
+                        np.amax(data[i, j + 1:width]) < tree or
+                        np.amax(data[0:i, j]) < tree or
+                        np.amax(data[i + 1:height, j]) < tree):
+                    count += 1
+
+    return count
+
+
+def solve_part_2(data):
+    scenic = []
+    height = data.shape[0]
+    width = data.shape[1]
+    for i in range(height):
+        for j in range(width):
+            tree = data[i][j]
+            ranges = [
+                np.flip(data[i, 0:j]),
+                data[i, j + 1:width],
+                np.flip(data[0:i, j]),
+                data[i + 1:height, j]
+            ]
+            tree_scenic = []
+            for r in ranges:
+                count = 0
+                for v in r:
+                    count += 1
+                    if v >= tree:
+                        break
+                tree_scenic.append(count)
+            scenic.append(np.prod(tree_scenic))
+    return max(scenic)
+
+
+if __name__ == "__main__":
+    data = load_and_parse_data(DAY)
+    print("Part 1:", solve_part_1(data))
+    print("Part 2:", solve_part_2(data))
---
+++ day8_test.py
@@ -0,0 +1,20 @@
+import unittest
+import day8 as puzzle
+
+DAY = 8
+RESULT_PART_1 = 21
+RESULT_PART_2 = 8
+
+
+class TestDaay(unittest.TestCase):
+    def test_part1(self):
+        if RESULT_PART_1 is None:
+            self.skipTest("No result for part 1")
+        data = puzzle.load_and_parse_data(DAY, True)
+        self.assertEqual(puzzle.solve_part_1(data), RESULT_PART_1)
+
+    def test_part2(self):
+        if RESULT_PART_2 is None:
+            self.skipTest("No result for part 2")
+        data = puzzle.load_and_parse_data(DAY, True)
+        self.assertEqual(puzzle.solve_part_2(data), RESULT_PART_2)
