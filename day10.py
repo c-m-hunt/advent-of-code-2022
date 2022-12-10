515cb10ecbf5f28f025def55e04889bc36514a4a
Chris Hunt <chris.hunt@hudl.com>
2022-12-10 09:54:30 +0000

    feat: day 10

---
+++ day10.py
@@ -0,0 +1,51 @@
+from typing import List
+import numpy as np
+import utils
+
+DAY = 10
+
+
+def load_and_parse_data(day: int, test: bool = False) -> List[str]:
+    data = utils.get_input(day, test)
+    return [l.split(" ") for l in data]
+
+
+def get_strengths(data):
+    strengths = [1]
+
+    for instruction in data:
+        prev_strength = strengths[-1]
+        if len(instruction) == 1:
+            strengths.append(prev_strength)
+            continue
+        if instruction[0] == "addx":
+            amount = int(instruction[1])
+            strengths.append(prev_strength)
+            strengths.append(prev_strength + amount)
+    return strengths
+
+
+def solve_part_1(data):
+    sum_strengths = [20, 60, 100, 140, 180, 220]
+    strengths = get_strengths(data)
+
+    return sum([idx * strengths[idx - 1] for idx in sum_strengths])
+
+
+def solve_part_2(data):
+    strengths = get_strengths(data)
+    display = np.zeros(6 * 40)
+    for i, s in enumerate(strengths):
+        j = i % 40
+        if j - 1 <= s <= j + 1:
+            display[i] = 1
+    display = display.reshape(6, 40)
+    for row in range(display.shape[0]):
+        print("".join(["X" if int(display[row, col]) ==
+              1 else " " for col in range(display.shape[1])]))
+
+
+if __name__ == "__main__":
+    data = load_and_parse_data(DAY)
+    print("Part 1:", solve_part_1(data))
+    print("Part 2:", solve_part_2(data))
---
+++ day10_test.py
@@ -0,0 +1,12 @@
+import unittest
+import day10 as puzzle
+DAY = 10
+RESULT_PART_1 = 13140
+
+
+class TestDaay(unittest.TestCase):
+    def test_part1(self):
+        if RESULT_PART_1 is None:
+            self.skipTest("No result for part 1")
+        data = puzzle.load_and_parse_data(DAY, True)
+        self.assertEqual(puzzle.solve_part_1(data), RESULT_PART_1)
