524039429ef6b4b934fff5a9bb158c217214ed24
Chris Hunt <chris.hunt@hudl.com>
2022-12-11 14:36:36 +0000

    feat: better reporting of results

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import sys
 
 DAY = sys.argv[1]
 
-puzzle = importlib.import_module("advent2022.day" + DAY)
+puzzle = importlib.import_module("advent2022.day" + str(DAY))
 
 if __name__ == "__main__":
+    print("----------")
+    print("Day:", DAY)
+    print("----------")
     data = puzzle.load_and_parse_data(DAY)
     print("Part 1:", puzzle.solve_part_1(data))
     data = puzzle.load_and_parse_data(DAY)
     print("Part 2:", puzzle.solve_part_2(data))
+    print("----------")
