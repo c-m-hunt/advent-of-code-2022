875c99ae73f8371f5c8d758841d3e7971c71ff1e
Chris Hunt <chris.hunt@hudl.com>
2022-12-13 09:20:02 +0000

    feat: day 12 part 1

--- advent2022/day12.py
+++ advent2022/day12.py
@@ -1,9 +1,6 @@
-from typing import List, Tuple, Optional
+from typing import List
 import numpy as np
-
 from advent2022 import utils
-import sys
-# sys.setrecursionlimit(5000)
 
 
 def load_and_parse_data(day: int, test: bool = False) -> List[str]:
@@ -42,90 +39,49 @@ move = {
     "right": lambda x: (x[0], x[1] + 1),
 }
 
-points_visited = None
-route = []
-
 
 def valid_move(grid, from_point, to_point):
-    # try:
     if 0 > to_point[0] or to_point[0] >= grid.shape[0]:
         return False
     from_point_value = char_map.get(
         grid[from_point[0], from_point[1]], None)
     to_point_value = char_map.get(grid[to_point[0], to_point[1]], None)
-    if grid[to_point[0], to_point[1]] in ["S", "E"]:
-        return True
-    if from_point_value - to_point_value <= 1:
+    if from_point_value - to_point_value >= -1:
         return True
     return False
-    # except Exception:
-    #     return False
-
-
-routes = []
-
-
-def get_directions(grid, point):
-    directions = {
-        "up": grid[point[0] - 1, point[1]] if point[0] - 1 >= 0 else None,
-        "down": grid[point[0] + 1, point[1]] if point[0] + 1 < grid.shape[0] else None,
-        "left": grid[point[0], point[1] - 1] if point[1] - 1 >= 0 else None,
-        "right": grid[point[0], point[1] + 1] if point[1] + 1 < grid.shape[1] else None,
-    }
-
-    dir_vals = {
-        "up": char_map.get(directions["up"], None),
-        "down": char_map.get(directions["down"], None),
-        "left": char_map.get(directions["left"], None),
-        "right": char_map.get(directions["right"], None),
-    }
-
-    sorted_directions = dict(sorted(
-        dir_vals.items(), key=lambda x: x[1] if x[1] else 0))
-    return sorted_directions.keys()
 
 
 def solve_map(grid, start, target):
-
     calc_grid = np.zeros(grid.shape)
-    visited_grid = np.zeros(grid.shape)
-    visited_grid[target[0], target[1]] = 1
     moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-    def recurse(point):
-        for m in moves:
-            new_point = (point[0] + m[0], point[1] + m[1])
-            if (0 > new_point[0] or new_point[0] >= grid.shape[0]
-                    or 0 > new_point[1] or new_point[1] >= grid.shape[1]):
-                continue
-            if visited_grid[new_point[0], new_point[1]] == 1:
-                continue
-            if valid_move(grid, point, new_point):
-                calc_grid[new_point[0], new_point[1]
-                          ] = calc_grid[point[0], point[1]] + 1
-                visited_grid[new_point[0], new_point[1]] = 1
-
-                recurse(new_point)
-            print(calc_grid)
-    recurse(target)
-    print(calc_grid)
-    return calc_grid[start[0], start[1]]
+    moves_to_test, next_moves_to_test, step = [start], [], 0
+    while True:
+        step += 1
+        for m_f in moves_to_test:
+            for m_t in moves:
+                new_point = (m_f[0] + m_t[0], m_f[1] + m_t[1])
+                if (0 > new_point[0] or new_point[0] >= grid.shape[0]
+                        or 0 > new_point[1] or new_point[1] >= grid.shape[1]):
+                    continue
+                if calc_grid[new_point[0], new_point[1]] != 0:
+                    continue
+                if grid[new_point[0], new_point[1]] == "S":
+                    continue
+                if valid_move(grid, m_f, new_point):
+                    calc_grid[new_point[0], new_point[1]
+                              ] = step
+                    next_moves_to_test.append(new_point)
+                    if new_point == target:
+                        print(calc_grid)
+                        return calc_grid[target[0], target[1]]
+        moves_to_test = next_moves_to_test.copy()
+        next_moves_to_test = []
 
 
 def solve_part_1(data):
     start, end = get_start_and_end_points(data)
-    print(start, end)
-    global points_visited
-    points_visited = np.zeros(data.shape)
-    # try:
     resolved = solve_map(data, start, end)
-    print(resolved)
-    print(route)
-    print("Length", len(routes))
-    print(min([len(r) for r in routes]))
-    # except Exception as e:
-    #     print(e)
-    return min([len(r) for r in routes])
+    return resolved
 
 
 def solve_part_2(data):
